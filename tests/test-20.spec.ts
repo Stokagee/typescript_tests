cbaad1b98f253885dd85b11c880ed1a6178b081b
Dušan Čižmarik <stokagecizmarik@gmail.com>
2026-05-11 12:20:27 +0200

    Confirmation + Prompt alert done

--- tests/test-20.spec.ts
+++ tests/test-20.spec.ts
@@ -15,3 +15,41 @@ test("Testing JS alerts - Simple alert", async ({page}) => {
 
 
 })
+
+test("Testing JS alerts - Confirmation alert", async ({page}) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    page.on('dialog', (dialog) => {
+        console.log("Dialog type", dialog.type());
+        expect(dialog.type()).toContain("confirm");
+        console.log("Dialog text", dialog.message());
+        expect(dialog.message()).toContain("Press a button!");
+        dialog.accept();
+    });
+
+    await page.locator("#confirmBtn").click()
+    const msgText: Locator = page.locator("#demo");
+    await expect(msgText).toContainText("You pressed OK!")
+
+
+})
+
+test.only("Testing JS alerts - Prompt alert", async ({page}) => {
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    page.on('dialog', (dialog) => {
+        console.log("Dialog type", dialog.type());
+        expect(dialog.type()).toContain("prompt");
+        console.log("Dialog text", dialog.message());
+        expect(dialog.message()).toContain("Please enter your name:");
+        expect(dialog.defaultValue()).toContain("Harry Potter");
+
+        dialog.accept("John");
+    });
+
+    await page.locator("#promptBtn").click()
+    const msgText: Locator = page.locator("#demo");
+    await expect(msgText).toContainText("John")
+
+
+})
